7415d6b79a9e705028e247c4d3cd80c8a4afbdb9
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2020-10-16 13:53:20 +0900

    .

--- stockToKakao/config/config.py
+++ stockToKakao/config/config.py
@@ -8,8 +8,8 @@ def chromeDriverPath():
     if ip_name == "localServer":
         chromeDriverPath = "C:/Users/rtjxo/PycharmProjects/stockToKakao/stockToKakao/chromedriver/chromedriver_windows.exe"
     elif ip_name == "awsServer":
-        chromeDriverPath = "/home/ubuntu/projects/stockToKakao/chromedriver/chromedriver_ubuntu"
+        chromeDriverPath = "/home/ubuntu/project_01/stockToKakao/chromedriver/chromedriver_ubuntu"
     else:
-        chromeDriverPath = "/home/ubuntu/projects/stockToKakao/chromedriver/chromedriver_ubuntu"
+        chromeDriverPath = "/home/ubuntu/project_01/stockToKakao/chromedriver/chromedriver_ubuntu"
 
     return chromeDriverPath
