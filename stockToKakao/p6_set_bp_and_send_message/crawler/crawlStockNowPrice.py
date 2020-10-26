129181f53da7dbe7c84e1c4ce4e5feb2bdb0045a
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2020-10-26 10:29:32 +0900

    .

---
+++ stockToKakao/p6_set_bp_and_send_message/crawler/crawlStockNowPrice.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import logging
+import requests
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from stockToKakao.chromedriver.setPageDriver import set_page_driver
+
+
+# 한건의 data 추출
+def getStockNowPrice(stc_id):
+    # 로거
+    logger = logging.getLogger(__name__)
+
+    try:
+        # 데이터 탐색
+        url = "https://finance.naver.com/item/main.nhn?code=" + str(stc_id)
+        page_call_result = requests.get(url)
+        bs_obj = BeautifulSoup(page_call_result.content.decode('euc-kr', 'replace'), "html.parser")
+
+        # driver = set_page_driver(url)
+        # bs_obj = BeautifulSoup(driver.page_source, 'html.parser')
+        # driver.close()
+
+        # 현재가정보
+        contentInfo = bs_obj.find("div", {"id": "content"}).find("p", {"class": "no_today"}).find("em")
+
+        # 현재가
+        now_price = contentInfo.find_all("span")[0].text.replace(",", "")
+
+        # 필요 데이터 추출
+        result_value = {"now_price": now_price}
+
+        return result_value
+    except Exception as ex:
+        logger.error("ERROR!!!!: getStockNowPrice")
+        logger.error(ex)
+
+
+if __name__ == '__main__':
+    print(getStockNowPrice('005930'))
---
+++ stockToKakao/p6_set_bp_and_send_message/setBpAndSendMessage.py
@@ -0,0 +1,98 @@
+import json
+import requests
+import sys
+import os
+from datetime import datetime
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from stockToKakao.commonModule import dbModule
+from stockToKakao.p6_set_bp_and_send_message.crawler.crawlStockNowPrice import getStockNowPrice
+
+
+# 헤더세팅
+def set_headers():
+    # 토큰조회
+    db_class = dbModule.Database()
+    sql = "SELECT access_token from stock_search.kakao_token"
+    row = db_class.executeOne(sql)
+
+    # 헤더세팅
+    headers = {
+        "Authorization": "Bearer " + row['access_token']
+    }
+    return headers
+
+
+# 데이터세팅
+def set_data(stc_id, stc_name):
+    data = {
+        "template_object": json.dumps({"object_type": "text",
+                                       "text": "전고점 돌파!!\n "
+                                               "["+stc_name+"]\n"
+                                               " https://finance.naver.com/item/main.nhn?code="+stc_id,
+                                       "link": {
+                                           "web_url": "https://www.daum.com/",
+                                           "mobile_web_url": "https://www.daum.com/"},
+                                       "button_title": "네이버증권 바로가기"
+                                       })}
+    print('\n송신대상: '+stc_id+"["+stc_name+"]")
+    return data
+
+
+# 메시지송신
+def send_message(headers, data):
+    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+    response = requests.post(url, headers=headers, data=data)
+    if response.json().get('result_code') == 0:
+        print('메시지를 성공적으로 보냈습니다.')
+    else:
+        print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ' + str(response.json()))
+
+
+def main_process():
+    # # 헤더세팅
+    # headers = set_headers()
+
+    # 당일
+    today = datetime.today().strftime("%Y%m%d")
+    time = datetime.today().strftime("%H%M%S")
+
+    print(today, time)
+    # # DB 모듈선언
+    # db_class = dbModule.Database()
+    #
+    # # 당일 기 수행된 데이타가 있다면 clear
+    # sql = "DELETE from stock_search.stock_captured WHERE capture_tcd = '01'AND capture_dt = '%s'" % today
+    # db_class.execute(sql)
+    # db_class.commit()
+    #
+    # # 대상건 조회
+    # sql = "SELECT stc_id, stc_name from stock_search.stock_basic where filter_yn = 'Y'"
+    # rows = db_class.executeAll(sql)
+    #
+    # # 조회된 건수 바탕으로 판별 및 송신
+    # for row in rows:
+    #     # 판별대상 데이터
+    #     stc_id = row['stc_id']
+    #     stc_name = row['stc_name']
+    #
+    #     # 판별 및 전송
+    #     if capture_stock(stc_id):
+    #
+    #         # 데이터세팅
+    #         data = set_data(stc_id, stc_name)
+    #
+    #         # 결과저장
+    #         sql = "insert into stock_search.stock_captured (capture_dt, stc_id ,capture_tcd ) " \
+    #               "values( '%s','%s','01')" % (today, stc_id)
+    #         db_class.execute(sql)
+    #         db_class.commit()
+    #
+    #         # 메시지송신
+    #         send_message(headers, data)
+    #
+    # print("\n메시지 송신 완료")
+
+
+if __name__ == "__main__":
+    main_process()
