ae118368720f8da5fbaaa57936a49a647895f926
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2020-12-09 23:41:46 +0900

    .

--- stockToKakao/p4_capture_and_send_message/bizLogic/decisionPossibleStock.py
+++ stockToKakao/p4_capture_and_send_message/bizLogic/decisionPossibleStock.py
@@ -12,19 +12,19 @@ def decision_possible_stock(stc_id):
 
     # 거래량 없으면 0리턴
     if tdy_prices_info['dealAmt'] == 0 or bf1_prices_info['dealAmt'] == 0 or bf2_prices_info['dealAmt'] == 0:
-        return {"cls_price": 0, "dealQnt": 0}
+        return {"cls_price": 0, "low_price": 0, "dealQnt": 0}
 
     # 1전거래일 상승 아니면 0리턴
     if float(bf1_prices_info['opn_price']) >= float(bf1_prices_info['cls_price']):
-        return {"cls_price": 0, "dealQnt": 0}
+        return {"cls_price": 0, "low_price": 0, "dealQnt": 0}
 
     # 전거래일 거래량이 2전거래일 거래량보다 500% 상승 안했으면 0리턴
     if bf1_prices_info['dealAmt'] < bf2_prices_info['dealAmt']*5:
-        return {"cls_price": 0, "dealQnt": 0}
+        return {"cls_price": 0, "low_price": 0, "dealQnt": 0}
 
     # 당일 거래량이 전거래일 거래량보다 25% 이상 하락 안했으면 0리턴
     if tdy_prices_info['dealAmt'] > bf1_prices_info['dealAmt']/4:
-        return {"cls_price": 0, "dealQnt": 0}
+        return {"cls_price": 0, "low_price": 0, "dealQnt": 0}
 
     # 당일 종가가 5일 평균 하회 하면 0리턴
     avg_5 = (float(tdy_prices_info['cls_price']) +
@@ -36,8 +36,8 @@ def decision_possible_stock(stc_id):
         return {"cls_price": 0, "dealQnt": 0}
 
     # 끝까지 모든 조건 충족시 당일종가 및 전 거래량 리턴
-    return {"cls_price": float(tdy_prices_info['cls_price']), "dealQnt": bf1_prices_info['dealQnt']}
+    return {"cls_price": float(tdy_prices_info['cls_price']), "low_price": float(tdy_prices_info['low_price']), "dealQnt": bf1_prices_info['dealQnt']}
 
 
 if __name__ == '__main__':
-    print(decision_possible_stock('000590'))
+    print(decision_possible_stock('241690'))
--- stockToKakao/p4_capture_and_send_message/possibilityToIncrease.py
+++ stockToKakao/p4_capture_and_send_message/possibilityToIncrease.py
@@ -40,13 +40,14 @@ def main_process():
             # 판별 및 전송 (decision_possible_stock 은 조건에 맞으면 종가를 리턴하고 조건에 안맞으면 0을 리턴한다.)
             deal_info = decision_possible_stock(stc_id)
             price = float(deal_info['cls_price'])
+            low_price = float(deal_info['low_price'])
             deal_qnt = deal_info['dealQnt']
             roq = round((deal_qnt / float(row['num_of_circulation'])) * 100, 2)
 
             if price > 0:
                 # 결과저장
-                sql = "insert into stock_search.stock_captured (capture_dttm, stc_id, price, rate_of_quant, capture_tcd, msg ) " \
-                      "values('%s', '%s', '%d', '%f', '01', '%s')" % (now_time, stc_id, price, roq, '상승가능성 종목확인!!')
+                sql = "insert into stock_search.stock_captured (capture_dttm, stc_id, price, low_price, rate_of_quant, capture_tcd, msg ) " \
+                      "values('%s', '%s', '%d', '%f', '01', '%s')" % (now_time, stc_id, price, low_price, roq, '상승가능성 종목확인!!')
                 db_class.execute(sql)
                 db_class.commit()
 
