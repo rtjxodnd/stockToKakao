1df28d2e300a4ed3d8ad0c03b98b839d12c5f59b
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2020-12-26 15:08:37 +0900

    .

--- stockToKakao/p0_send_message/send_telegram_message.py
+++ stockToKakao/p0_send_message/send_telegram_message.py
@@ -1,9 +1,15 @@
 import telegram
 telgm_token = '1464479777:AAFhQAL7l7beuWbJivYbReXNEHqfePYlFr4'
 bot = telegram.Bot(token=telgm_token)
-updates = bot.getUpdates()
-print(updates)
-for i in updates:
-    print(i)
+
+
+bot.sendMessage(chat_id = '-1001243985347', text="챗봇 테스트")
+
+
+
+# updates = bot.getUpdates()
+# print(updates)
+# for i in updates:
+#     print(i)
 
 print('start telegram chat bot')
