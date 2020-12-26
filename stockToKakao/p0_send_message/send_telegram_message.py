f808176e9bfa5406ad4d81466c8370de5dd4eee0
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2020-12-26 14:14:08 +0900

    .

--- stockToKakao/p0_send_message/send_telegram_message.py
+++ stockToKakao/p0_send_message/send_telegram_message.py
@@ -1,5 +1,10 @@
 import telegram
+
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 token = '1464479777:AAFhQAL7l7beuWbJivYbReXNEHqfePYlFr4'
+
+print(token)
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 bot = telegram.Bot(token=token)
 
 for i in bot.getUpdates():
